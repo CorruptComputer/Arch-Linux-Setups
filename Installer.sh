862ebea825a851543569b213c02d4782de931a4d
Nickolas Gupton <nickolas@gupton.xyz>
2019-11-14 20:27:26 -0600

    Script the partitioning process

--- Installer.sh
+++ Installer.sh
@@ -51,64 +51,55 @@ else
   echo "Continuing with default $DISK..."
 fi
 
-echo "Please partition the disk..."
-echo "Make 3 partitions: 1 primary +1G, EFI System; 2 primary -8G, Linux Filesystem; 3 primary, Linux Swap."
+
+echo "Partitioning the disk..."
+echo "Making 3 partitions: 1 primary +1G, EFI System; 2 primary -8G, Linux Filesystem; 3 primary, Linux Swap."
 echo "....................................................."
-fdisk $DISK
+# Found here: https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
+# to create the partitions programatically (rather than manually)
+# we're going to simulate the manual input to fdisk
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $DISK
+  g   # GPT partition table
+  n   # new partition
+  1   # partition number 1
+      # default beginning
+  +1G # end 1GB later
+  t   # change type
+  1   # type 1, EFI System
+  n   # new partition
+  2   # partition number 2
+      # default beginning
+  -8G # end 8GB before the end of the disk
+  n   # new partition
+  3   # partition number 3
+      # default beginning
+      # default ending
+  t   # change partition type
+  3   # partition number 3
+  19  # type 19, Linux Swap
+  w   # write disk
+EOF
 echo "....................................................."
 echo "Here is the partition table for $DISK..."
 fdisk -l $DISK
 
 echo "....................................................."
-echo ""
-while [ true ]; do
-  echo "Where do you want the EFI System? "
-  read answer
-
-  if [[ -n "$answer" ]]; then
-    mkfs.fat -F32 $answer
-    break
-  else
-    echo "Please enter a partition."
-    continue
-    
-  fi
-done
-
+echo "EFI System being created here: $DISK"1
+mkfs.fat -F32 "$DISK"1
 echo "....................................................."
-echo ""
-while [ true ]; do
-  echo "Where do you want the root filesystem? "
-  read answer
-
-  if [[ -n "$answer" ]]; then
-    mkfs.ext4 $answer
-    ROOT=$answer
-    break
-  else
-    echo "Please enter a partition."
-    continue
-    
-  fi
-done
-
+echo "Linux Filesystem being created here: $DISK"2
+mkfs.ext4 "$DISK"2
 echo "....................................................."
-echo ""
-while [ true ]; do
-  echo "Where do you want the swap? "
-  read answer
+echo "Linux Swap being created here: $DISK"3
+mkswap "$DISK"3
+swapon "$DISK"3
+echo "....................................................."
 
-  if [[ -n "$answer" ]]; then
-    mkswap $answer
-    swapon $answer
-    break
-  else
-    echo "Please enter a partition."
-    continue
-  fi
-done
-
-mount $ROOT /mnt
+mount "$DISK"2 /mnt
 
 pacstrap /mnt base base-devel linux linux-firmware gnome gdm vim sudo grub efibootmgr dosfstools
 
